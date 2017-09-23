8b3879ac51be87503679e33a36a7a1d3f1112b4e
Amit Prakash <aprakash@internal.tavisca.com>
2017-09-23 17:23:18 +0530

    added lucene project

---
+++ Lucene/Lucene.Indexing/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Indexing;
+
+namespace Lucene.Indexing
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Airlines> airlineList = Airlines.GetSampleData();
+            CustomLuceneSearch.AddUpdateLuceneIndex(airlineList);
+            while (true)
+            {
+                Console.WriteLine("Enter your search query...");
+                string querry = Console.ReadLine();
+                IEnumerable<Airlines> airlineResults = CustomLuceneSearch.Search(querry);
+                if (airlineResults.Count<Airlines>() > 0)
+                {
+                    Console.WriteLine($"{airlineResults.Count<Airlines>()} Record(s) Found\n");
+                    int i = 1;
+                    foreach (Airlines airline in airlineResults)
+                    {
+                        Console.WriteLine($"Record ({i++})\n");
+                        Console.WriteLine($"Name : {airline.Name}\nIata Identification : {airline.IataIdentifier}\nDescription : {airline.Description}");
+                    }
+                }
+                else Console.WriteLine("No such record found");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+
+}
